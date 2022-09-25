250c305b38a8b0d04f5360ca8ae78cee8427df65
bruno lessa <brunolessa@live.com>
2022-09-25 20:01:10 -0300

    create home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
