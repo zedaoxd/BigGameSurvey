963fa5027641ab172241e86f14e28ea10cb2c499
bruno lessa <brunolessa@live.com>
2022-09-25 11:03:58 -0300

    Project created

---
+++ backend/src/main/java/com/biggamesurvey/biggamesurvey/BigGameSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.biggamesurvey.biggamesurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigGameSurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigGameSurveyApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/biggamesurvey/biggamesurvey/BigGameSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biggamesurvey.biggamesurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigGameSurveyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
