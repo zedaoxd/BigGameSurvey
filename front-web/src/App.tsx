9c7ef1c63152e9b1e5519e96f095e4b565bc3541
bruno lessa <brunolessa@live.com>
2022-09-25 19:47:09 -0300

    add header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <h1>Hello</h1>;
+  return <Header />;
 }
 
 export default App;
